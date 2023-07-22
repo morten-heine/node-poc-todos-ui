d296710e651114ff284a9b4b8c5dd5827e0cf13b
Morten Heine Sørensen <mhs@formalit.dk>
2023-07-22 13:55:45 +0200

    Replacing all classes by functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Todos from './components/TodosPage/Todos';
 import TodoComments from './components/TodoCommentsPage/TodoComments';
 
+export const AppContext = createContext();
+
 function App() {
+    const [showCompleted, setShowCompleted] = useState(false);
+
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Todos />} />
-                <Route path="/todos/:id/comments" element={<TodoComments />} />
-            </Routes>
-        </Router>
+        <AppContext.Provider value={{ showCompleted, setShowCompleted }}>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Todos />} />
+                    <Route path="/todos/:id/comments" element={<TodoComments />} />
+                </Routes>
+            </Router>
+        </AppContext.Provider>
     );
 }
 
--- src/components/TodosPage/Todos.js
+++ src/components/TodosPage/Todos.js
@@ -1,117 +1,99 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import TodoCreator from './subcomponents/TodoCreator/TodoCreator';
 import TodoRow from './subcomponents/TodoRow/TodoRow';
 import './Todos.css';
 import { getApiBaseUrl } from '../../utils/config.js';
+import { AppContext } from '../../App';
 
-class Todos extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: [],
-            showCompleted: false
-        };
-        this.baseUrl = getApiBaseUrl();
-    }
+const Todos = () => {
+    const [todos, setTodos] = useState([]);
+    const { showCompleted, setShowCompleted } = useContext(AppContext);
+    const baseUrl = getApiBaseUrl();
 
-    componentDidMount = async () => {
-        try {
-            const response = await fetch(`${this.baseUrl}/todos`);
-            const todos = await response.json();
-            this.setState({ todos:todos });
-
-        } catch(error) {
-            console.error("Failed to fetch todos:", error);
-            this.setState({ todos: [] });
+    useEffect(() => {
+        const fetchTodos = async () => {
+            try {
+                const response = await fetch(`${baseUrl}/todos`);
+                const todos = await response.json();
+                setTodos(todos);
+            } catch(error) {
+                console.error("Failed to fetch todos:", error);
+                setTodos([]);
+            }
         }
-    }
+        fetchTodos();
+    }, []);
 
-    addTodo = async (newText) => {
-        const response = await fetch(`${this.baseUrl}/todos/`, {
+    const addTodo = async (newText) => {
+        const response = await fetch(`${baseUrl}/todos/`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ text: newText, done:false })
         });
         const newTodo = await response.json();
-        this.setState({
-            todos: [...this.state.todos, newTodo]
-        });
+        setTodos(prevTodos => [...prevTodos, newTodo]);
     }
 
-    markTodoDone = async (id) => {
-        await fetch(`${this.baseUrl}/todos/${id}/done`, { method: 'POST' });
-        this.setState({
-            todos: this.state.todos.map(todo => {
-                if (todo.id === id) {
-                    return {
-                        ...todo,
-                        done: true
-                    };
-                }
-                return todo;
-            })
-        });
-    }
-
-    markTodoUndone = async (id) => {
-        await fetch(`${this.baseUrl}/todos/${id}/undone`, { method: 'POST' });
-        this.setState({
-            todos: this.state.todos.map(todo => {
-                if (todo.id === id) {
-                    return {
-                        ...todo,
-                        done: false
-                    };
-                }
-                return todo;
-            })
-        });
-    }
-
-    toggleShowCompleted = () => {
-        this.setState({ showCompleted: !this.state.showCompleted });
-    }
-
-    render() {
-        const undoneTodos = Array.isArray(this.state.todos) ? this.state.todos.filter(todo => !todo.done) : [];
-        const doneTodos = Array.isArray(this.state.todos) ? this.state.todos.filter(todo => todo.done) : [];
-
-        return (
-            <div className="header">
-                <h1>Todo List: {undoneTodos.length} items left</h1>
-                <TodoCreator onAddTodo={this.addTodo} />
-                <div className="display-completed">
-                    <label>
-                        <input type="checkbox" onChange={this.toggleShowCompleted} id="displayCompletedCheckbox"/> Display completed items
-                    </label>
-                </div>
-                <div className="todo-list">
-                    <h2>Undone</h2>
-                    {undoneTodos.map(todo => (
-                        <TodoRow
-                            key={todo.id}
-                            todo={todo}
-                            markTodoDone={this.markTodoDone}
-                            markTodoUndone={this.markTodoUndone}
-                        />
-                    ))}
-                    {this.state.showCompleted && (
-                        <>
-                            <h2>Done</h2>
-                            {doneTodos.map(todo => (
-                                <TodoRow
-                                    key={todo.id}
-                                    todo={todo}
-                                    markTodoDone={this.markTodoDone}
-                                    markTodoUndone={this.markTodoUndone}
-                                />
-                            ))}
-                        </>
-                    )}
-                </div>
-            </div>
+    const markTodoDone = async (id) => {
+        await fetch(`${baseUrl}/todos/${id}/done`, { method: 'POST' });
+        setTodos(prevTodos =>
+            prevTodos.map(todo =>
+                todo.id === id ? { ...todo, done: true } : todo
+            )
         );
     }
+
+    const markTodoUndone = async (id) => {
+        await fetch(`${baseUrl}/todos/${id}/undone`, { method: 'POST' });
+        setTodos(prevTodos =>
+            prevTodos.map(todo =>
+                todo.id === id ? { ...todo, done: false } : todo
+            )
+        );
+    }
+
+    const toggleShowCompleted = () => {
+        setShowCompleted(!showCompleted);
+    }
+
+    const undoneTodos = todos.filter(todo => !todo.done);
+    const doneTodos = todos.filter(todo => todo.done);
+
+    return (
+        <div className="header">
+            <h1>Todo List: {undoneTodos.length} items left</h1>
+            <TodoCreator onAddTodo={addTodo} />
+            <div className="display-completed">
+                <label>
+                    <input type="checkbox" checked={showCompleted} onChange={toggleShowCompleted} id="displayCompletedCheckbox"/> Display completed items
+                </label>
+            </div>
+            <div className="todo-list">
+                <h2>Undone</h2>
+                {undoneTodos.map(todo => (
+                    <TodoRow
+                        key={todo.id}
+                        todo={todo}
+                        markTodoDone={markTodoDone}
+                        markTodoUndone={markTodoUndone}
+                    />
+                ))}
+                {showCompleted && (
+                    <>
+                        <h2>Done</h2>
+                        {doneTodos.map(todo => (
+                            <TodoRow
+                                key={todo.id}
+                                todo={todo}
+                                markTodoDone={markTodoDone}
+                                markTodoUndone={markTodoUndone}
+                            />
+                        ))}
+                    </>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default Todos;
