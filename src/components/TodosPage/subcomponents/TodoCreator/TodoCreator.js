1fb2ebf0301ee3f3edb0c08e310e099158db62e2
Morten Heine Sørensen <mhs@formalit.dk>
2023-07-22 14:04:37 +0200

    Replacing all classes by functions

--- src/components/TodosPage/subcomponents/TodoCreator/TodoCreator.js
+++ src/components/TodosPage/subcomponents/TodoCreator/TodoCreator.js
@@ -1,32 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../../../App.css';
 
-class TodoCreator extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            input: ""
-        };
+const TodoCreator = (props) => {
+    const [input, setInput] = useState("");
+
+    const handleInputChange = (event) => {
+        setInput(event.target.value);
     }
 
-    handleInputChange = (event) => {
-        this.setState({ input: event.target.value });
+    const handleAddTodo = async () => {
+        props.onAddTodo(input);
+        setInput("");
     }
 
-
-    handleAddTodo = async () => {
-        this.props.onAddTodo(this.state.input);
-        this.setState({ input: "" });
-    }
-
-    render() {
-        return (
-            <div className="input-button-wrapper">
-                <input type="text" onChange={this.handleInputChange} value={this.state.input} id="addText"/>
-                <button onClick={this.handleAddTodo} id="addButton" className="button">Add</button>
-            </div>
-        );
-    }
+    return (
+        <div className="input-button-wrapper">
+            <input type="text" onChange={handleInputChange} value={input} id="addText"/>
+            <button onClick={handleAddTodo} id="addButton" className="button">Add</button>
+        </div>
+    );
 }
 
 export default TodoCreator;
